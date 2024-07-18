b8855f418de5d39def8235241a2c86b8eacf94dc
William Woodard <williamwoodard98@gmail.com>
2024-07-18 12:01:23 -0500

    installed pillow, add url pattern, changed settings

--- cookprojectproject/settings.py
+++ cookprojectproject/settings.py
@@ -133,3 +133,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
--- cookprojectproject/urls.py
+++ cookprojectproject/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

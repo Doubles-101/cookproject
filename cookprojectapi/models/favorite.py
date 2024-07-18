2cc8b862308494e8885b07049d6fbc4e184cde6e
William Woodard <williamwoodard98@gmail.com>
2024-07-18 11:58:27 -0500

    completed all but recipeimages, need to install pillow

--- cookprojectapi/models/favorite.py
+++ cookprojectapi/models/favorite.py
@@ -3,5 +3,5 @@ from .customer import Customer
 from .recipe import Recipe
 
 class Favorite(models.Model):
-    customer = models.ForeignKey(Customer, related_name='customer', on_delete=models.DO_NOTHING,)
-    recipe = models.ForeignKey(Recipe, related_name='recipe', on_delete=models.DO_NOTHING,)
+    customer = models.ForeignKey(Customer, related_name='favorites', on_delete=models.DO_NOTHING,)
+    recipe = models.ForeignKey(Recipe, related_name='favorite_by', on_delete=models.DO_NOTHING,)
--- cookprojectapi/models/review.py
+++ cookprojectapi/models/review.py
@@ -3,7 +3,7 @@ from .customer import Customer
 from .recipe import Recipe
 
 class Review(models.Model):
-    customer = models.ForeignKey(Customer, related_name='customer', on_delete=models.DO_NOTHING,)
-    recipe = models.ForeignKey(Recipe, related_name='recipe', on_delete=models.DO_NOTHING,)
+    customer = models.ForeignKey(Customer, related_name='reviews', on_delete=models.DO_NOTHING,)
+    recipe = models.ForeignKey(Recipe, related_name='review_by', on_delete=models.DO_NOTHING,)
     comment = models.CharField(max_length=300)
     create_date = models.DateField(default="0000-00-00",)

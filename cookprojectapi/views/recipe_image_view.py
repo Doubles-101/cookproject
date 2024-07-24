e7f1931e4d9db8a579b98649ef269b1f1de60167
William Woodard <williamwoodard98@gmail.com>
2024-07-24 14:10:39 -0500

    try block for retrieving an image

--- cookprojectapi/views/recipe_image_view.py
+++ cookprojectapi/views/recipe_image_view.py
@@ -18,11 +18,13 @@ class PictureViewSet(ViewSet):
         pass
 
     def retrieve(self, request, pk=None):
-
-        picture = RecipeImage.objects.get(recipe_id=pk)
-        serializer = PictureSerializer(picture, context={'request': request})
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
+        try:
+            picture = RecipeImage.objects.get(recipe_id=pk)
+            serializer = PictureSerializer(picture, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        except RecipeImage.DoesNotExist:
+            return Response({"error": "Recipe Image id does not exist"}, status=status.HTTP_200_OK)
 
     def create(self, request):
         try:
